5ff8317189c5610bd22233937fa23ff38a61bec6
pirhona2 <anton.pirhonen@aalto.fi>
2021-03-30 15:25:27 +0300

    Update performance tests

--- performance_tests.py
+++ performance_tests.py
@@ -4,21 +4,21 @@ import sys
 sys.path.append('/usr/local/lib/python2.7/site-packages/')
 import RNA
 import time
-fwrite = open("performance_test_results.txt", "a")
+fwrite = open("performance_test_results.csv", "a")
 fread  = open("performance_test_sequences.txt", "r")
 seq = fread.readline()
-fwrite.write("Sequence,SecondaryStructure,Runtime,GCcontent,GCdistance,StructureDistance\n")
+fwrite.write("Sequence,SecondaryStructure,Length,Runtime,GCcontent,GCdistance,StructureDistance\n")
 while len(seq) != 0:
 	seq = seq.replace("\n", "")
 	if (seq[0] == "#"):
 		seq = fread.readline()
 		continue
 	fwrite.write(seq + ",")
-	fwrite.flush()
 	time.sleep(.5)
 	gc = GCcount.GCcount(seq)
 	seq = RNA.fold(seq)[0]
 	fwrite.write(seq + ",")
+	fwrite.write(str(len(seq)) + ",")
 	fwrite.flush()
 	os.system("python2 MCTS-RNA.py -s \"" + seq + "\" -GC " + str(gc) + " -d 0.02 -pk 0")
 	seq = fread.readline()
